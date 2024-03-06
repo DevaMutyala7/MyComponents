0630ea52b1013f43851687933001a2e6d1c3c2f8
Deva <devateja58@gmail.com>
2024-03-06 14:16:48 +0530

    ft:added script files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,12 @@
-import packageJSON from "./package.json";
+import packageJSON from "./package.json" assert { type: "json" };
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.tsx",
     output: [
       {
         file: packageJSON.main,
